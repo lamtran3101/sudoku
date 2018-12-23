c1db14338d85fb09529f8f3a2dd6fa8921ec3c16
tranlam <tran@commonpeoplei.com>
2018-12-23 19:51:30 +0100

    change pot

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = process.env.PORT || 3000;
+
 // viewed at http://localhost:8080
 app.use(express.static('dist'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(80);
+app.listen(port);
